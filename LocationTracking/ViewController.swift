563e87ef4aac1ddfabeb64f8ccaccbdd9b9d5faf
김종현 <jhkim@dit.ac.kr>
2017-10-23 20:14:53 +0900

    1st commit

--- LocationTracking/ViewController.swift
+++ LocationTracking/ViewController.swift
@@ -7,19 +7,34 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var myMapView: MKMapView!
+    
+    var locationManager: CLLocationManager!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestAlwaysAuthorization()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+        // 지도에 현재 위치 마크를 보여줌
+        myMapView.showsUserLocation = true
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let coor = manager.location?.coordinate
+        print("latitute" + String(describing: coor?.latitude) + "/ longitude" + String(describing: coor?.longitude))
     }
-
 
 }
 
